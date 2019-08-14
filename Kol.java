02c173cf0350688ad2f14065bb0e6dca272461b4
dpienczyn <donia19881@wp.pl>
2019-08-14 12:45:44 +0200

    Add files via upload

--- Kol.java
+++ Kol.java
@@ -12,85 +12,86 @@ public class Kol {
 		char z;
 		Scanner s = new Scanner(System.in);
 		String k;
+		System.out.println();
+		System.out.println();
+		System.out.println("    1 2 3");
+		System.out.println("a   " + tab[0][0]+ "|" + tab[0][1]+ "|" + tab[0][2]);
+		System.out.println("b   " + tab[1][0]+ "|" + tab[1][1]+ "|" + tab[1][2]);
+		System.out.println("c   " + tab[2][0]+ "|" + tab[2][1]+ "|" + tab[2][2]);
+		System.out.println();
+		System.out.println("Wprowadzaj do tablicy dane wpisujac: a1 c2 b3 itp");
 		for(int i=0; i<9; i++) {
+		System.out.println();
+		System.out.println("Wprowadz dana: ");		
 		k = s.nextLine();
 		if(gracz%2==0) {
-			if(k.charAt(0)=='1'){
+			if(k.charAt(0)=='a'&&k.charAt(1)=='1'){
 				if(tab[0][0]==' ') {
 				tab[0][0]='X';
-				System.out.println(tab[0][0]);
 				}else {
 					System.out.println("Wybierz inne pole");
 				gracz--;
 				}
 			}
-			if(k.charAt(0)=='2') {
+			if(k.charAt(0)=='a'&&k.charAt(1)=='2') {
 				if(tab[0][1]==' ') {
 				tab[0][1]='X';
-				System.out.println(tab[0][1]);
 				}else {
 					System.out.println("Wybierz inne pole");
 				gracz--;
 				}
 			}
-			if(k.charAt(0)=='3') {
+			if(k.charAt(0)=='a'&&k.charAt(1)=='3') {
 				if(tab[0][2]==' ') {
 				tab[0][2]='X';
-				System.out.println(tab[0][2]);
 				}else {
 					System.out.println("Wybierz inne pole");
 				gracz--;
 				}
 			}
-			if(k.charAt(0)=='4') {
+			if(k.charAt(0)=='b'&&k.charAt(1)=='1') {
 				if(tab[1][0]==' ') {
 				tab[1][0]='X';
-				System.out.println(tab[1][0]);
 				}else {
 					System.out.println("Wybierz inne pole");
 				gracz--;
 				}
 			}
-			if(k.charAt(0)=='5') {
+			if(k.charAt(0)=='b'&&k.charAt(1)=='2') {
 				if(tab[1][1]==' ') {
 				tab[1][1]='X';
-				System.out.println(tab[1][1]);
 				}else {
 					System.out.println("Wybierz inne pole");
 				gracz--;
 				}
 			}
-			if(k.charAt(0)=='6') {
+			if(k.charAt(0)=='b'&&k.charAt(1)=='3') {
 				if(tab[1][2]==' ') {
 				tab[1][2]='X';
-				System.out.println(tab[1][2]);
 				}else {
 					System.out.println("Wybierz inne pole");
 				gracz--;
 				}
 			}
-			if(k.charAt(0)=='7') {
+			if(k.charAt(0)=='c'&&k.charAt(1)=='1') {
 				if(tab[2][0]==' ') {
 				tab[2][0]='X';
-				System.out.println(tab[2][0]);
 				}else {
 					System.out.println("Wybierz inne pole");
 				gracz--;
 				}
 			}
-			if(k.charAt(0)=='8') {
+			if(k.charAt(0)=='c'&&k.charAt(1)=='2') {
 				if(tab[2][1]==' ') {
 				tab[2][1]='X';
-				System.out.println(tab[2][1]);
 				}else {
 					System.out.println("Wybierz inne pole");
 				gracz--;
 				}
 			}
-			if(k.charAt(0)=='9') {
+			if(k.charAt(0)=='c'&&k.charAt(1)=='3') {
 				if(tab[2][2]==' ') {
 				tab[2][2]='X';
-				System.out.println(tab[2][2]);
 				}else {
 					System.out.println("Wybierz inne pole");
 				gracz--;
@@ -98,90 +99,81 @@ public class Kol {
 			}
 			gracz++;
 		}else if(gracz%2!=0) {
-			if(k.charAt(0)=='1') {
+			if(k.charAt(0)=='a'&&k.charAt(1)=='1') {
 				if(tab[0][0]==' ') {
 				tab[0][0]='O';
-				System.out.println(tab[0][0]);
 				}
 				else {
 					System.out.println("Wybierz inne pole");
 				gracz--;
 				}
 			}
-			if(k.charAt(0)=='2') {
+			if(k.charAt(0)=='a'&&k.charAt(1)=='2') {
 				if(tab[0][1]==' ') {
 				tab[0][1]='O';
-				System.out.println(tab[0][1]);
 				}
 				else {
 					System.out.println("Wybierz inne pole");
 				gracz--;
 				}
 			}
-			if(k.charAt(0)=='3') {
+			if(k.charAt(0)=='a'&&k.charAt(1)=='3') {
 				if(tab[0][2]==' ') {
 				tab[0][2]='O';
-				System.out.println(tab[0][2]);
 				}else {
 					System.out.println("Wybierz inne pole");
 				gracz--;
 				}
 						
 			}
-			if(k.charAt(0)=='4') {
+			if(k.charAt(0)=='b'&&k.charAt(1)=='1') {
 				if(tab[1][0]==' ') {
 				tab[1][0]='O';
-				System.out.println(tab[1][0]);
 				}else {
 					System.out.println("Wybierz inne pole");
 				gracz--;
 				}
 				
 			}
-			if(k.charAt(0)=='5') {
+			if(k.charAt(0)=='b'&&k.charAt(1)=='2') {
 				if(tab[1][1]==' ') {
 				tab[1][1]='O';
-				System.out.println(tab[1][1]);
 				}else {
 					System.out.println("Wybierz inne pole");
 				gracz--;
 				}
 					
 			}
-			if(k.charAt(0)=='6') {
+			if(k.charAt(0)=='b'&&k.charAt(1)=='3') {
 				if(tab[1][2]==' ') {
 				tab[1][2]='O';
-				System.out.println(tab[1][2]);
 				}else {
 					System.out.println("Wybierz inne pole");
 				gracz--;
 				}
 				
 			}
-			if(k.charAt(0)=='7') {
+			if(k.charAt(0)=='c'&&k.charAt(1)=='1') {
 				if(tab[2][0]==' ') {
 				tab[2][0]='O';
-				System.out.println(tab[2][0]);
 				}else {
 					System.out.println("Wybierz inne pole");
 				gracz--;
 				}
 				
 			}
-			if(k.charAt(0)=='8') {
+			if(k.charAt(0)=='c'&&k.charAt(1)=='2') {
 				if(tab[2][1]==' ') {
 				tab[2][1]='O';
-				System.out.println(tab[2][1]);
 				}else {
 					System.out.println("Wybierz inne pole");
 				gracz--;
 				}
 				
 			}
-			if(k.charAt(0)=='9') {
+			if(k.charAt(0)=='c'&&k.charAt(1)=='3') {
 				if(tab[2][2]==' ') {
 				tab[2][2]='O';
-				System.out.println(tab[2][2]);
 				}else {
 					System.out.println("Wybierz inne pole");
 				gracz--;
@@ -189,6 +181,14 @@ public class Kol {
 			}
 			gracz++;
 		}
+		
+		System.out.println();
+		System.out.println();
+		System.out.println("    1 2 3");
+		System.out.println("a   " + tab[0][0]+ "|" + tab[0][1]+ "|" + tab[0][2]);
+		System.out.println("b   " + tab[1][0]+ "|" + tab[1][1]+ "|" + tab[1][2]);
+		System.out.println("c   " + tab[2][0]+ "|" + tab[2][1]+ "|" + tab[2][2]);
+		
 		if(tab[0][0]=='X'&&tab[0][1]=='X'&&tab[0][2]=='X') {
 			System.out.println("Wygra³ krzy¿yk");
 		}else if(tab[0][0]=='O'&&tab[0][1]=='O'&&tab[0][2]=='O') {
